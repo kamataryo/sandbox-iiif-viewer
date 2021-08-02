82dfa95204cb73fd0ea2719ba3813b09d2dfb3fe
kamataryo <kamataryo@users.noreply.github.com>
2021-08-02 11:57:00 +0900

    Init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import { Viewer } from "react-iiif-viewer"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Viewer iiifUrl="https://so69jnd8td.execute-api.ap-northeast-1.amazonaws.com/latest/iiif/2/test/info.json" />
       </header>
     </div>
   );
