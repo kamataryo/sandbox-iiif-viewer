caf4023bc2138ce31e9c2e117d037a94647524ab
kamataryo <kamataryo@users.noreply.github.com>
2021-08-03 19:25:11 +0900

    Load id key

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
-import { Viewer } from "react-iiif-viewer"
+import "./App.css";
+import { Viewer } from "react-iiif-viewer";
+import { useEffect, useState } from "react";
+
+const baseApiUrl =
+  "https://xt5bkm0oee.execute-api.ap-northeast-1.amazonaws.com/dev/main";
+const putItemUrl =
+  "https://xt5bkm0oee.execute-api.ap-northeast-1.amazonaws.com/dev/url";
+const urlSearchParams = new URLSearchParams(window.location.search);
+const id = urlSearchParams.get("id");
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Viewer iiifUrl="https://so69jnd8td.execute-api.ap-northeast-1.amazonaws.com/latest/iiif/2/test/info.json" />
+        <Viewer
+          iiifUrl={`https://so69jnd8td.execute-api.ap-northeast-1.amazonaws.com/latest/iiif/2/${id}/info.json`}
+        />
       </header>
     </div>
   );
